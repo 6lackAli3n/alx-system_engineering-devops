cf30927d7ad1002037885e607f3da224e6bfddb4
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-03-13 05:48:17 +0100

    token

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,2 +1,13 @@
 #!/usr/bin/env ruby
-#puts ARGV[0].scan(/hbt{2,5}n/).join
+# Ensure that there's exactly one argument provided
+unless ARGV.length == 1
+  puts "Usage: #{$PROGRAM_NAME} <string>"
+  exit(1)
+end
+
+regex = /hb{0,1}tn/
+
+match = ARGV[0].match(regex)
+
+# Output the matched string if there's a match, otherwise print nothing
+puts match ? match[0] : ""
