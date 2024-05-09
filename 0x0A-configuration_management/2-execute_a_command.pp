08be622968152147453ba133fa790a68759d5c4b
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-05-09 01:46:33 +0100

    pup

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,7 +1,7 @@
 # Puppet manifest to kill a process named "killmenow"
 
 exec { 'pkill':
-command     => 'pkill -9 -f killmenow || true',
-path        => ['/usr/bin', '/usr/sbin', '/bin'],
-refreshonly => true,
+  command     => 'pkill -9 -f killmenow || true',
+  path        => ['/usr/bin', '/usr/sbin', '/bin'],
+  refreshonly => true,
 }
