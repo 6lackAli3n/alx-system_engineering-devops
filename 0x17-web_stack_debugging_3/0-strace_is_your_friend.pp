a1a25a353d9bf7478f54b6033579229b6bd25eb9
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-06-21 01:08:49 +0100

    lint

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,6 +1,6 @@
 # This Puppet manifest ensures that the required directory for Apache is present and has the correct permissions
 
-exec { 'create-uploads-dir':
-  command => '/bin/mkdir -p /var/www/html/uploads && /bin/chown -R www-data:www-data /var/www/html/uploads',
-  unless  => '/bin/test -d /var/www/html/uploads',
+exec { 'fix-wordpress':
+command => 'sed -i s/phpp/php/g /var/www/html/wp-settings.php',
+path    => '/usr/local/bin/:/bin/'
 }
