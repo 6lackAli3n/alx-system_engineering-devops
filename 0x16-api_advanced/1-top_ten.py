570c2a979374e067205d79ea4f7fa127078f0f2a
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-06-22 04:09:56 +0100

    top

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -16,13 +16,16 @@ def top_ten(subreddit):
     """
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
     headers = {'User-Agent': 'custom_user_agent'}
+    params = {'limit': 10}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        posts = data['data']['children']
-        for post in posts:
-            print(post['data']['title'])
+    try:
+        if response.status_code == 200:
+            data = response.json()
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
         else:
-            print(None)
+            print("None")
+    except requests.RequestException:
+        print("None")
