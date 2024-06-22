b61b1de00673cbff95af4db7fb6fec5a47881ccc
6lackAli3n <abubakarabdullahayo26@gmail.com>
2024-06-22 03:29:02 +0100

    subs

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -20,10 +20,13 @@ def number_of_subscribers(subreddit):
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
     headers = {'User-Agent': 'custom_user_agent'}
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code == 200:
         data = response.json()
         return data['data']['subscribers']
     else:
+        return 0
+    except requests.RequestException:
         return 0
